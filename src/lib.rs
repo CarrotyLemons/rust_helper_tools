8fcfc43814e1cf897dac7c772be468d8a3f84893
CarrotyLemons <david@CarrotyLemons.local>
2025-04-20 00:22:28 +0930

    Added no-std Floats trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+pub trait Floats:
+    Sub<Output = Self>
+    + SubAssign
+    + Add<Output = Self>
+    + AddAssign
+    + Neg<Output = Self>
+    + Mul<Output = Self>
+    + MulAssign
+    + Div<Output = Self>
+    + DivAssign
+    + Copy
+    + Sized
+    + PartialOrd
+{
+}
+
+//impl Floats for f16 {}
+impl Floats for f32 {}
+impl Floats for f64 {}
+//impl Floats for f128 {}
+
 #[macro_export]
 macro_rules! assert_flexible {
     ($value1:expr, $value2:expr, $percent:expr) => {{
